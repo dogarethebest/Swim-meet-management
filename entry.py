76ce1c3de985616f82eb68455ab4de6f629abf8b
dogarethebest <212955596+dogarethebest@users.noreply.github.com>
2025-07-17 16:35:41 +0000

    modified:   entry.py new file:   program_File/scripps/create_meet.py

--- entry.py
+++ entry.py
@@ -1,19 +1,27 @@
-print("beginning start up")
-
-
 import json
 import time
+import sqlite3
 
 with open('program_File/Data/build_info.json', "r") as file:
     data = json.load(file)
     build_id = data.get("build_id")
+    author = data.get("author")
     Version = data.get("version")
+    build_id = data.get("build_id")
+
 
 
 print("this software is still currently in Beta.")
-
-time.sleep(0.5)
-
 print(f"Version:{Version}")
 print(f"build id:{build_id}")
+print(f"made by:{author}")
+print(f"build ID:{build_id}")
 
+print("would you like to create a new meet?")
+print("or would you like to resume or start a existing meet?")
+
+x = input("(yes for new meet no for existing meet)")
+
+
+if(x == "yes"):
+    print("Creating meet")
