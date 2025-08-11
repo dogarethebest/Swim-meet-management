cc3568878f00d7317321e207960c520c47e53d88
nicholas <212955596+dogarethebest@users.noreply.github.com>
2025-08-11 11:40:55 -0400

    modified:   entry.py renamed:    program_File/scripps/create_meet.py -> program_File/scripps/create_meet_api.py

--- entry.py
+++ entry.py
@@ -1,3 +1,18 @@
-print("swim meet management")
-v = 2
-print(f"V:{v}")
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+build_id = data.get('build_id')
+project_name = data.get('project_name')
+version = data.get('version')
+author = data.get('author')
+release_date = data.get('release_date')
+post_build_int = data.get('post_build_int')
+build_ID_dot = data.get('build_ID.')
+
+print(build_id)
+print(project_name)
+print(version)
+print(author)
+print(release_date)
+print(post_build_int)
+print(build_ID_dot)
